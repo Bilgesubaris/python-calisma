9d96756011ea36c395b005285c9d39d083a151fa
Bilgesu Barış <117085455+Bilgesubaris@users.noreply.github.com>
2025-01-22 20:23:53 +0300

    dosyaveriatama.py

--- dosyaveriatama.py
+++ dosyaveriatama.py
@@ -28,3 +28,28 @@ with open("bolunur.txt", "w") as f:
   f.write(" ".join(str(bolunur) for bolunur in bolunurler))
 
 print("İşlem tamamlandı! Asal sayılar 'asal.txt' ve asal olmayanlar 'bolunur.txt' dosyasına kaydedildi.")
+
+def sayi_olustur(dosya):
+    with open('dosya.txt','w') as file:
+     sayi= random.sample(range(1, 101), 100)
+     file.write(f"{sayi}\n")
+
+def asal_mi(sayi):
+    for i in range (0,sayi):
+      if sayi%i==0:
+        return False
+      else:
+        return True
+
+def dosya_yaz(dosya,asal,bolunur):
+ with open('dosya','r') as file:
+  sayilar =dosya.readlines()
+  with open('asal','w') as asal:
+   with open('bolunur','w') as asal:
+
+    for sayi in sayilar:
+      sayi=int(sayi.strip())
+      if asal_mi(sayi):
+        asal.write(f"{sayi}\n")
+      else :
+       bolunur.write(f"{sayi}\n")
